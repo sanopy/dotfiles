3ad461e5c19fca45b9aad9cc127c0ebbf6dc5185
uoo38 <plus.wisteria38@gmail.com>
2018-04-07 02:12:53 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,13 @@
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 
 # setup for dein.vim
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
+mkdir -p ~/.vim/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s ~/.vim/dein
+
+# setup for Rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+rustup component add rustfmt-preview
 
 # setup for ac-php
 sudo apt update
